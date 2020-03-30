c2ce8af0e1795ad7300d11d9ba95304a5696fa62
Ali Parvizi <snparvizi75@gmial.com>
2020-03-30 15:58:51 +0430

    sample secret.py

---
+++ hemayat/secrets-sample.py
@@ -0,0 +1,13 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '446eyo@*@6(%vq1pba5n3#m)yy5)hv@8g#+$ypwwqr#b!8r(2_'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
